b49ed519caa2079cb9ac3f348013b070f86769a3
Samuelito660 <163178624+Samuelito660@users.noreply.github.com>
2025-10-07 23:46:13 +0200

    terza-bozza

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     $allergie = isset($_POST["allergie"]) ? $_POST["allergie"] : [];
 
         // Usare in_array($ago, $pagliaio)
-        // --> ritorna true se l'aglo è nel pagliaio
+        // --> ritorna true se l'ago è nel pagliaio
         // --> ritorna false se l'ago NON è nel pagliaio
 ?>
 
@@ -65,17 +65,33 @@
                     [
                         "nome" => "Pasta al pesto",
                         "img" => "https://www.google.com/imgres?q=pasta%20al%20pesto&imgurl=https%3A%2F%2Fcookingwithelo.com%2Fwp-content%2Fuploads%2F2022%2F06%2FPasta-al-pesto.jpg&imgrefurl=https%3A%2F%2Fcookingwithelo.com%2Fpasta-al-pesto%2F&docid=fBjqS1DqwAkHsM&tbnid=AOFX3wjLe4clcM&vet=12ahUKEwi3hpLmgY-QAxW_8gIHHbQyGmMQM3oECCoQAA..i&w=1200&h=1200&hcb=2&ved=2ahUKEwi3hpLmgY-QAxW_8gIHHbQyGmMQM3oECCoQAA",
-                        "allergeni" =>["uova", "glutine", "fruttasecca"]
+                        "allergeni" => ["uova", "glutine", "fruttasecca"]
+
+                    ],
+                    
+                    [
+                        "nome" => "Pizza Margherita",
+                        "img" => "https://www.google.com/imgres?q=pizza%20margherita&imgurl=https%3A%2F%2Fcookingitalians.com%2Fwp-content%2Fuploads%2F2024%2F11%2FMargherita-Pizza.jpg&imgrefurl=https%3A%2F%2Fcookingitalians.com%2Fauthentic-margherita-pizza-recipe-italian-classic%2F&docid=DjP0xU2LEjWkSM&tbnid=T-qTfWLP7MhTkM&vet=12ahUKEwjL9_-ehJOQAxVOxQIHHWCIJGsQM3oECB0QAA..i&w=1344&h=768&hcb=2&ved=2ahUKEwjL9_-ehJOQAxVOxQIHHWCIJGsQM3oECB0QAA",
+                        "allergeni" => ["uova", "glutine", "fruttasecca", "lattosio", ]
+
+                    ],
+                    
+                    [
+                        "nome" => "Insalata mista",
+                        "img" => "https://www.google.com/imgres?q=insalata%20mista&imgurl=https%3A%2F%2Fwww.nadamasristorante.it%2Fpublic%2Fbox%2Fimmagini%2F294717%2Finsalata-mista.jpg&imgrefurl=https%3A%2F%2Fwww.nadamasristorante.it%2Fc%2Fcontorni%2Finsalata-mista-grande-25659.html&docid=YDT2cXLi0Yg0LM&tbnid=K2C8UhwPaeLj3M&vet=12ahUKEwjOjZDUhJOQAxWZxgIHHf2PCyAQM3oECBUQAA..i&w=800&h=500&hcb=2&ved=2ahUKEwjOjZDUhJOQAxWZxgIHHf2PCyAQM3oECBUQAA",
+                        "allergeni" => [""]
+
+                    ],
+                    
+                    [
+                        "nome" => "Risotto ai funghi",
+                        "img" => "https://www.google.com/imgres?q=risotto%20ai%20funghi&imgurl=http%3A%2F%2Fwww.speziata.it%2Fwp-content%2Fuploads%2F2017%2F09%2Frisotto-ai-funghi.jpg&imgrefurl=http%3A%2F%2Fwww.speziata.it%2Frecipe%2Frisotto-ai-funghi%2F&docid=oabCb8UOkV82mM&tbnid=ffcz7lFQ-g_kRM&vet=12ahUKEwjumIvohJOQAxXr0QIHHYzCNdMQM3oECCYQAA..i&w=1280&h=720&hcb=2&ved=2ahUKEwjumIvohJOQAxXr0QIHHYzCNdMQM3oECCYQAA",
+                        "allergeni" => ["uova", "glutine", "fruttasecca", "lattosio"]
 
                     ],
 
-                    [
-
-                    ]
+                    
                 ];
-
-
-                $ago = false ;
 
                 if ($_SERVER['REQUEST_METHOD'] == "POST") {
                     if ($nome) {
@@ -85,27 +101,31 @@
                         }
                 
                 
-                    if (!empty($allergie)) {
-                        foreach($allergie as $allergia) {
-                            echo "<p>Sei allergico a: <strong>$allergia</strong></p>";
-                            if(in_array($allerigia, $ricetta["allergeni"]) !== false){
+                    foreach ($ricette as $ricetta) {
+                        $ago = false ;
+                        foreach ($allergie as $allergia) {
+                            if (in_array($allergia, $ricetta["allergeni"])) {
                                 $ago = true;
                             }
                         }
-                    } else {
-                        echo "<p>Non hai segnalato allergie.</p>";
                     }
 
-                    if ($ago){
-                        echo "<p>Attenzione! Il piatto <strong>$piatto</strong> potrebbe contenere allergeni per te.</p>";
+                    if ($ago) {
+                        echo "<p>Attenzione! Il piatto $piatto contiene allergeni che hai segnalato.</p>";
                     } else {
-                        echo "<p>Il piatto <strong>$piatto</strong> è sicuro per te.</p>";
+                        echo "<p>Il piatto $piatto è sicuro per te!</p>";
                     }
-                    
+
+
+
+
+
+
+
                 }
 
-            ?>
 
+            ?>
         </div>
 
     </form>
